5b187f163949f64353023ba7d25a6079053df805
Natalya Bakhtina <nbakhtina87@gmail.com>
2025-12-05 22:31:11 +0300

    files from lesson dependencies

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log('Hello, Hexlet!')
+//импорт бибиотеки лодаш
+import _ from 'lodash'
+//пересечение массивов
+console.log(_.intersection([2,1],[2,3]))
+//capitalize делает первую букву заглавной
+console.log(_.capitalize('hello'))
+console.log(_.last(['one','two']))
