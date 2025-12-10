c2f065e7a04bf26ec2bca764e9ef6f97f3d51e85
Natalya Bakhtina <nbakhtina87@gmail.com>
2025-12-10 20:24:07 +0300

    update 3 files lesson NPX

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-console.log('Hello, Hexlet!')
+console.log("Hello, Hexlet!");
 //импорт бибиотеки лодаш
-import _ from 'lodash'
+import _ from "lodash";
 //пересечение массивов
-console.log(_.intersection([2,1],[2,3]))
+console.log(_.intersection([2, 1], [2, 3]));
 //capitalize делает первую букву заглавной
-console.log(_.capitalize('hello'))
-console.log(_.last(['one','two']))
+console.log(_.capitalize("hello"));
+console.log(_.last(["one", "two"]));
